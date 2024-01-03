67045e5d96cb22948cd1282239c50b10fc807720
Patrick Culmer <CulmerPatrick@gmail.com>
2024-01-03 01:00:54 +0000

    Initial Commit

---
+++ BugWorld/BugWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  BugWorldApp.swift
+//  BugWorld
+//
+//  Created by Patrick Culmer on 03/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct BugWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BugWorld/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BugWorld
+//
+//  Created by Patrick Culmer on 03/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
